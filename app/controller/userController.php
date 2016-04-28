6316cf72092f595b1f3a78642511c51108c10164
Pablo salvador Baguena Camarena <pabaca@floridauniversitaria.es>
2016-04-28 12:27:06 +0200

    Push de prueba

--- app/controller/userController.php
+++ app/controller/userController.php
@@ -2,11 +2,21 @@
 
 require 'app/model/Usuarios.class.php';
 
-function registrarUsuario() {
+//"Funcion" de Registrar Usuario
+if(isset($_POST["register"])) {
 	$usuario = new Usuario();
 	$usuario->registrarUsuario($_POST["username"], $_POST["password"], $_POST["email"]);
 }
-
-function loginUsuario() {}
+//"Funcion" de Login Usuario
+elseif(isset($_POST["login"])) {}
+//"Funcion" de Editar Usuario
+elseif(isset($_POST["editUser"])) {
+	$usuario = new Usuario();
+	$usuario->actualizarUsuario($_POST["newPassword"], $_POST["newEmail"], $currentUser);//Falta algo per a traurer el currentUser
+}
+//En caso de que no se seleccione nada de lo anterior(Muy poco probable de que pase).
+else{
+	//Display algún error.
+}
 
 ?>
--- app/model/Usuarios.class.php
+++ app/model/Usuarios.class.php
@@ -5,17 +5,31 @@ include_once("Database.class.php");
 class Usuario extends Database {
 
 	//Insertar Usuario en la base de datos.
-	function registrarUsuario($username, $password, $email) {
-		$this->conectar();	
-		$sql = "INSERT INTO usuarios VALUES ('$username', '$password', '$email')";	
-		if($query = $this->consulta($sql)){
-			$this->desconectar();	
+	function registrarUsuario($username, $password, $email)	{
+		$this->conectar();
+		$sql = "INSERT INTO usuarios VALUES ('$username', '$password', '$email')";
+		if ($query = $this->consulta($sql)) {
+			$this->desconectar();
 			return true;
 		}
 		else {
-			$this->desconectar();	
+			$this->desconectar();
 			return false;
 		}
+	}
+
+	function actualizarUsuario($newPassword, $newEmail, $user) {
+		$this->conectar();
+		$sql = "UPDATE usuarios SET password='$newPassword', email='$newEmail' WHERE username=$user";
+		if ($query = $this->consulta($sql)) {
+			$this->desconectar();
+			return true;
+		}
+		else {
+			$this->desconectar();
+			return false;
+		}
+	}
 }
 
 ?>
